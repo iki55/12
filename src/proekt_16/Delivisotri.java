e91c54209c8d7c2fb2b33955ef4d19146a0cebbb
iki55 <aleksovska.isidora@gmail.com>
2023-11-27 21:09:48 +0100

    Add first java program

---
+++ src/proekt_16/Delivisotri.java
@@ -0,0 +1,26 @@
+package proekt_16;
+import java.util.Scanner;
+public class Delivisotri {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		int i,n;
+				System.out.println ("Da se ispecatata;clanoi na niza koi se delivi so 3");
+		System.out.println("Vnesi broj na clenovi vo nizata");
+		int n=input.nextInt();
+		int niza []=new int(n);
+		System.out.println ("vnesi gi clenovite na nizata;");
+		for(int i=0;i<niza.lenght;i++) {
+			System.out.println("i++-->");
+			niza(i)=input.next.int();
+			}
+		
+		{
+		
+		}
+		
+	
+
+	}
+
+}
